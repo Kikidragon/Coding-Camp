8adbc0212587ee15040234a6fb6a1dbcff16d58c
Claire DeVlieger <claire@vincico.com>
2018-07-24 20:37:35 +0000

    first project

--- Magic8ball.py
+++ Magic8ball.py
@@ -1,8 +1,15 @@
 import random 
 x = ["Yes!", "No!", "Maybe?", "Decide for Yourself!", "No Comment.", "Never!", "Always!", "I can't answer that."]
 
-name = input("What is your name? ") 
-print("Hello",name,"!")
-print("What is your question for the Magic 8-Ball? ")
+print("Welcome to Magic 8 Ball!")
+name = raw_input("What is your name? ") 
+print("Hello!")
+y = raw_input("What is your question for the Magic 8-Ball? ")
 print("And your answer is...")
 print(random.choice(x))
+
+z = raw_input("Play again(y/n)? ")
+if z == "y":
+    print("Please rerun the program! :)")
+elif z == "n":
+    print("Have a good day :)")
