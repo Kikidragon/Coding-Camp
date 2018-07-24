b3813c7cb47b5c5d7024aede400cfbe5948bf66b
Claire DeVlieger <claire@vincico.com>
2018-07-24 14:26:54 +0000

    first program

---
+++ Helloworld.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("I am Claire")
+print("This is Python Coding Camp day 1")
+#this is a comment
+
+print 2+4
+print 2+(4+7)
+print 2**2
+print 10%3
+
+x = 3
+y = 2
+if x == y:
+    print ("Yay")
+else:
+        print ("Nay")
+        
+name = ("Claire")
+print name
